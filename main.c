a8ae306028357001692567005c2c2c8ad36e43fe
ryansyuan <ryansuduan@icloud.com>
2026-01-02 11:41:05 +0800

    Initial commit: Project structure setup

---
+++ main.c
@@ -0,0 +1,14 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+#include "smart_ledger.h"
+
+// Instantiate global variables
+Transaction ledger[100];
+int count = 0;
+
+int main() {
+    printf("Project Initialized.\n");
+    system("pause");
+    return 0;
+}
